71fba10a23f22e71b44ac7d5f11e8ec6bfedd4f8
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-02-21 14:13:02 -0400

    Commit alt

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,11 +1,19 @@
+import Link from "next/link";
+
 export default function Menu(){
     return(
         <header>
             <h1>Menu</h1>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Registrar</li>
+                    <Link href="/">
+                        <li>Home</li>
+                    </Link>
+
+                    <Link href="/registro">
+                        <li>Registrar</li>
+                    </Link>
+                    
                 </ul>
             </nav>
         </header>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+ import Menu from "./components/Menu";
+ import Footer from "./components/Footer";
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <Menu/>
+        {children}
+        <Footer/>
+      </body>
     </html>
-  )
+  );
 }
