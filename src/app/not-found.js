b91e7a5f1543d338f0967e96c27fcc30f2505b81
ThaissaMariana <thaissa2608mariana@gmail.com>
2024-03-06 10:39:54 -0400

    Commit agora está mais bonito

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,8 +4,9 @@ import styles from './components/not-found.module.css'
 export default function NotFound() {
     return (
         <div className={styles.card}>
-            <h2 className="">Página não encontrada</h2>
-            <Link href="/">Retornar para Home</Link>
+            <h2 className="">Página não encontrada!</h2>
+            <img className={styles.imagem} src="https://annymota.com/wp-content/uploads/2023/05/ERRO1-300x176.png" alt=""></img>
+            <a href="/">Retornar para Home</a>
         </div>
     );
 }
